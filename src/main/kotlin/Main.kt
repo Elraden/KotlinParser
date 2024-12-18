4563882b14390fc3ac81ecdb691de196bf90b94c
Elraden <morozovsa94@yandex.ru>
2024-12-18 20:49:41 +0300

    KS-5-8 added selector string, received quotes, added to one line with \n

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,18 @@ package org.example
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
 const val URI_STRING = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
 
 fun main() {
     val doc: Document = Jsoup.connect(URI_STRING).get()
-    println(doc.title())
+    val quoteCssSelector = ".bzpNIu"
+    val listQuote: Elements = doc.select(quoteCssSelector)
+    var quotesText = ""
+    for (quote: Element in listQuote) {
+        quotesText += "${quote.text()}\n"
+    }
+    println(quotesText)
 }
